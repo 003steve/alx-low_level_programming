16a93da5de3fa5a502d413c8844ea9bcb0e6e365
steve <stdama001@gmail.com>
2023-10-16 09:01:11 -0700

    Task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 {
 	int a, n, longi;
 
-	longi = 0;
+	i = 0;
 
 	for (a = 0; str[a] != '\0'; a++)
 		longi++;
@@ -17,7 +17,7 @@ void puts_half(char *str)
 	n = (longi / 2);
 
 	if ((longi % 2) == 1)
-		n = ((longi + 1) / 2;
+		n = ((longi + 1) / 2);
 
 	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
