b498ba191db86a84c2b17b682681a0775cd26237
steve <stdama001@gmail.com>
2023-10-23 10:30:15 -0700

    Task01

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchar(char *s. char c);
+char *_strchr(char *s. char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
@@ -12,4 +14,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif
+#endif/"MAIN_H"/
